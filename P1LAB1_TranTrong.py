5cd8e0c4708b424799838cd66a62ec9c3075edc2
tranttra818 <trongtra0311@gmail.com>
2026-09-13 19:29:43 -0400

    Add files via upload
    python program intro about get info from user

---
+++ P1LAB1_TranTrong.py
@@ -0,0 +1,20 @@
+# Trong Tran
+# 9/10/2026
+# Inputs and Outputs 
+
+# Get the first name
+first_name = input("Enter your first name: ")
+
+# Get the last name
+last_name = input("Enter your last name: ")
+
+# Output welcome message to the user 
+# Use concatenation in the output (+)
+print("Hello,", first_name, last_name + "!!! Welcome to CTI-110")
+print()
+print()
+print("Hello,", first_name, last_name + "!!!", "Welcome to CTI-110")
+print()
+print()
+print(f"Hello, {first_name} {last_name}!!! Welcome to CTI-110")
+
